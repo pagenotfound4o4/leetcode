decb678db6ab99ab442620ee0383d149f1658c7f
Unlucky <unlucky1990@gmail.com>
2015-06-04 15:43:40 +0800

    update 155

--- 155.java
+++ 155.java
@@ -1,43 +1,41 @@
 import java.util.*;
 
-public class Solution {
-    class MinStack {
-        Stack<Integer> minStack;
-        Stack<Integer> stack;
+class MinStack {
+    Stack<Integer> minStack;
+    Stack<Integer> stack;
 
-        public MinStack() {
-            stack = new Stack<Integer>();
-            minStack = new Stack<Integer>();
-        }
+    public MinStack() {
+        stack = new Stack<Integer>();
+        minStack = new Stack<Integer>();
+    }
 
-        public void push(int x) {
-            stack.push(x);
-            if (minStack.size() == 0) {
-                minStack.push(x);
-            } else {
-                minStack.push(Math.min(minStack.peek(), x));
-            }
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.size() == 0) {
+            minStack.push(x);
+        } else {
+            minStack.push(Math.min(minStack.peek(), x));
         }
+    }
 
-        public void pop() {
-            if (stack.size() > 0) {
-                stack.pop();
-                minStack.pop();
-            }
+    public void pop() {
+        if (stack.size() > 0) {
+            stack.pop();
+            minStack.pop();
         }
+    }
 
-        public int top() {
-            if (stack.size() == 0) {
-                return 0;
-            }
-            return stack.peek();
+    public int top() {
+        if (stack.size() == 0) {
+            return 0;
         }
+        return stack.peek();
+    }
 
-        public int getMin() {
-            if (minStack.size() == 0) {
-                return 0;
-            }
-            return minStack.peek();
+    public int getMin() {
+        if (minStack.size() == 0) {
+            return 0;
         }
+        return minStack.peek();
     }
 }
