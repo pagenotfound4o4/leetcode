c53d33c9f7fd2ef079d44542e05f57d45394e25e
Unlucky <unlucky1990@gmail.com>
2015-06-04 15:16:38 +0800

    add 219

---
+++ 219.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i=0; i<nums.length; i++) {
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
